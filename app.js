d0bbab2a0c2dcf5b083d87e95c5b10c40fd0a8c5
Ishan Pattnayak <ishanpattnayak@gmail.com>
2021-09-11 12:07:57 +0530

    Host

--- app.js
+++ app.js
@@ -161,4 +161,4 @@ app.use(function(req, res, next) {
 
 //-----Server----//
 let PORT = process.env.PORT || 3000;
-app.listen(3000, console.log(`Running on port ${PORT}`));
+app.listen(3000 ,'0.0.0.0', console.log(`Running on port ${PORT}`));
